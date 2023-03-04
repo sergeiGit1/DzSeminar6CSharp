d8164c6f19bc0d7ed8b8712428f65020c2565be8
sergei <dashkosergeiit@gmail.com>
2023-03-04 14:28:01 +0300

    homework is done

---
+++ task41/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+// больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Укажите сколько чисел хотите ввести: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числа через enter: ");
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        string input = Console.ReadLine();
+        int.TryParse(input, out array[i]);
+        array[i] = Convert.ToInt32(input);
+    }
+    return array;
+}
+
+int[] resultArray= GetArray(size);
+Console.WriteLine($"[{String.Join("; ",resultArray)}]");
+
+int count=0;
+for (int i = 0; i < resultArray.Length; i++)
+{
+    if (resultArray[i]>0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Вы ввели {count} положительных чисел");
---
+++ task42/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 42 (необязательно): Напишите программу, которая будет преобразовывать 
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите число: ");//Для отрицательных не делал т.к. доп задача :)
+int binary = Convert.ToInt32(Console.ReadLine());
+int[] arrayBinary = new int[GetSize(binary)];
+
+for (int i = 0; binary >= 1; i++)
+{
+    int temp = 0;
+    temp = binary % 2;
+    binary /= 2;
+    arrayBinary[i] = temp;
+}
+
+int GetSize(int binaryLocal)
+{
+    int count = 0;
+    for (int i = 0; binaryLocal >= 1; i++)
+    {
+        binaryLocal /= 2;
+        count++;
+    }
+    return count;
+}
+
+void ReverseArray(int[] arrayBinary)
+{
+    int j = arrayBinary.Length - 1;
+    for (int i = 0; i < arrayBinary.Length / 2; i++, j--)
+    {
+        int tempLocal = arrayBinary[i];
+        arrayBinary[i] = arrayBinary[j];
+        arrayBinary[j] = tempLocal;
+    }
+}
+ReverseArray(arrayBinary);
+Console.WriteLine($"Двоичное: {String.Join("", arrayBinary)}");
+
+
+
